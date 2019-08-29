969cfd6bd28047212a0beaa0d0d22c0d0c823c90
huyuanixn325 <huyuanxin0325@163.com>
2019-08-29 16:47:10 +0800

    crud操作“

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -2,11 +2,7 @@ package tws.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
 
@@ -21,9 +17,38 @@ public class EmployeeController {
     @Autowired
     private EmployeeMapper employeeMapper;
 
-    @GetMapping("")
+    @GetMapping
     public ResponseEntity<List<Employee>> getAll() {
         return ResponseEntity.ok(employeeMapper.selectAll());
     }
+
+    @GetMapping("/{employeeID}")
+    public ResponseEntity<Employee> selectOneEmployeeByID(@PathVariable String employeeID) {
+        Employee employee = employeeMapper.selectOneEmployee(employeeID);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/{employeeID}")
+    public ResponseEntity<Employee> updateEmployeeByID(@PathVariable String employeeID,@RequestBody Employee employee){
+        employeeMapper.updateEmployee(employeeID,employee);
+        employee.setId(employeeID);
+        return ResponseEntity.ok(employee);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+        String id = UUID.randomUUID().toString();
+        employee.setId(id);
+        System.out.println(employee.toString());
+        employeeMapper.addEmployee(employee);
+        return ResponseEntity.created(URI.create("/employees/"+id)).build();
+    }
+
+    @DeleteMapping("/{employeeID}")
+    public ResponseEntity<Employee> deleteEmployeeByID(@PathVariable String employeeID){
+        employeeMapper.deleteEmployee(employeeID);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
